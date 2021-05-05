c40e981e218cde66628025a6aed0290db770f6f6
Dmitry Frolov <dm.y.frolov@gmail.com>
2021-05-05 18:53:59 +0500

    Check datetime - GMT datetime string without [GMT] at the end

--- src/main/java/com/retail/backoffice/service/impl/RetailService.java
+++ src/main/java/com/retail/backoffice/service/impl/RetailService.java
@@ -356,7 +356,7 @@ public class RetailService implements IRetail {
 		List<CheckDetail> details = mapListCheckDetailToListDetail(checkDto.getDetails(), check);
 		changeProductRemainders(details, false);
 		detailRepo.saveAll(details);
-		return new DtoWithRetCode<>(checkId+","+checkDto.getDateTime().toString().split("[")[0]
+		return new DtoWithRetCode<>(checkId+","+checkDto.getDateTime().toString().split("\\[")[0]
 				, ReturnCodes.OK);
 	}
 
